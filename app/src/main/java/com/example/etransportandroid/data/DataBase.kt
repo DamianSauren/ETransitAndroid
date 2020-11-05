fabb968ada982b0e46849ad1377e1cae8e849e7b
Damian Sauren <damiansauren@gmail.com>
2020-11-05 16:31:08 +0100

    .

--- app/src/main/java/com/example/etransportandroid/data/DataBase.kt
+++ app/src/main/java/com/example/etransportandroid/data/DataBase.kt
@@ -1,28 +1,40 @@
 package com.example.etransportandroid.data
 
+import android.util.Log
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class DataBase {
 
-    object Users: Table() {
-        val id = varchar("id", 10)
-        val name = varchar("name", 50)
-
-        override val primaryKey = PrimaryKey(id, name = "PK_User_ID")
-    }
-
     fun insert() {
-        Database.connect("jdbc:postgresql://localhost:5432/ETransport", driver = "org.postgresql.Driver", user = "postgres", password = "password")
+        Database.connect("jdbc:h2:mem:ETransport", driver = "org.h2.Driver", user = "postgres", password = "password")
 
         transaction {
             addLogger(StdOutSqlLogger)
-            SchemaUtils.create(Users)
 
-            Users.insert {
-                it[id] = "damian"
-                it[name] = "Damian"
-            }
+            SchemaUtils.create(Cities)
+//
+//            //Insert new city
+//            val stPete = City.new {
+//                name = "St. Petersburg"
+//            }
+//
+                val results = City.all()
+            Log.d("Cities:", "result: $results")
         }
+    }
+
+    object Cities: IntIdTable() {
+        val name = varchar("name", 50)
+    }
+
+    class City(id: EntityID<Int>) : IntEntity(id) {
+        companion object: IntEntityClass<City>(Cities)
+
+        var name by Cities.name
     }
 }
